541d1173e0866f6c29f0d28dcd7c81ec59ebe2fa
Haozhe <zhz19980514@ufl.edu>
2020-07-22 19:18:27 -0400

    hello world

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def index():
     if request.method == 'POST':
         # img_pipeline()
         data = request.get_json()
-        img_pipeline()
+        # img_pipeline()
         return data['hello']
     else:
         return "Hello World"
