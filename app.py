6162d56565db7480e7dd62382d7c94b9d4934cfe
Haozhe <zhz19980514@ufl.edu>
2020-07-22 18:23:54 -0400

    deploy stuff to heroku

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask, request
+from modules.image_processor import img_pipeline
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
+    
     if request.method == 'POST':
-
+        img_pipeline()
         data = request.get_json()
         return data['hello']
     else:
--- modules/image_processor.py
+++ modules/image_processor.py
@@ -38,12 +38,13 @@ def img_pipeline():
     # img_nine = cv2.imread("images/opioid-bottle.jpg")  # Good
     # img_ten = cv2.imread("images/warfarin.jpg")  # Good
 
-    tr_exe_dir = os.path.join(os.path.abspath(
-        os.path.dirname((__file__))), "Tesseract-OCR/tesseract.exe")
-    print(tr_exe_dir)
-    print(os.path.exists(tr_exe_dir))
-    pytesseract.pytesseract.tesseract_cmd = tr_exe_dir
+    # tr_exe_dir = os.path.join(os.path.abspath(
+    #     os.path.dirname((__file__))), "Tesseract-OCR/tesseract.exe")
+    # print(tr_exe_dir)
+    # print(os.path.exists(tr_exe_dir))
+    # pytesseract.pytesseract.tesseract_cmd = tr_exe_dir
     img_list = []
+    pytesseract.pytesseract.tesseract_cmd = '/app/.apt/usr/bin/tesseract'
     # find the file with a .png extension
     for file in glob.glob('*.png'):
         img_list.append(file)
